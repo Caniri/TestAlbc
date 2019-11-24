c000c1e77c199038ec3856352ee9b4e4bd1c2856
ARegularWizard <58053137+ARegularWizard@users.noreply.github.com>
2019-11-24 16:06:18 -0500

    Muerte, Y Spawn.
    Creados los Scripts de Muerte y el de Spawn.

---
+++ ALBC/Assets/Contenidos/Scripts/Muerte.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Muerte : MonoBehaviour
+{
+
+    public GameObject jugador;
+
+    public Transform spawn;
+
+    public float constanteDeMuerte = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        jugador = GameObject.Find("Jugador");
+        spawn = GameObject.Find("Spawn").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (jugador.transform.position.y < -constanteDeMuerte)
+        {
+            Morir();
+        }
+    }
+
+    public void Morir()
+    {
+        jugador.transform.position = spawn.position;
+    }
+
+}
