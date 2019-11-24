b120624899037552c6e0d18b0645ac7c86240168
ARegularWizard <58053137+ARegularWizard@users.noreply.github.com>
2019-11-24 15:51:52 -0500

    Primera tanda de archivos
    Jugador
    Scripts de salto y movimiento
    Script de Camara

--- ALBC/Assets/Contenidos/Scripts/Movimiento.cs
+++ ALBC/Assets/Contenidos/Scripts/Movimiento.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public class Movimiento : MonoBehaviour
 {
+
+    public GameObject jugador;
+    public Rigidbody2D rb2d;
+
+    public float velocidadMaxima;
+    public float velocidad;
+    public float movimientoH;
+    public float potenciaSalto;
+
+
+    private void Awake()
+    {
+        jugador = GameObject.Find("Jugador");
+
+        rb2d = jugador.gameObject.GetComponent<Rigidbody2D>();
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +31,29 @@ public class Movimiento : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb2d.AddForce(new Vector2(0f, potenciaSalto));
+        }
+
+        Debug.Log(rb2d.velocity.x);
     }
+
+    private void FixedUpdate()
+    {
+
+        movimientoH = Input.GetAxisRaw("Horizontal");
+
+        Vector2 movilidad = new Vector2(movimientoH * velocidad, 0f);
+
+        rb2d.AddForce(movilidad);
+
+        rb2d.velocity = new Vector2(Mathf.Clamp(rb2d.velocity.x, -velocidadMaxima, velocidadMaxima), rb2d.velocity.y);
+
+
+
+    }
+
+
+
 }
